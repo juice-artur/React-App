5ad49cf6a1797ec564f5f3fd4aa4531251476a71
juice-artur <asasin.lol10@gmail.com>
2024-03-22 16:45:42 +0200

    Rework to sql style migration

--- backend/migrations/1711106069103-initial.ts
+++ backend/migrations/1711106069103-initial.ts
@@ -1,52 +1,23 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateTasksTable1711106069103 implements MigrationInterface {
-
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(new Table({
-            name: "tasks",
-            columns: [
-                {
-                    name: "id",
-                    type: "int",
-                    isPrimary: true,
-                    isGenerated: true,
-                    generationStrategy: "increment"
-                },
-                {
-                    name: "column_id",
-                    type: "int"
-                },
-                {
-                    name: "title",
-                    type: "varchar"
-                },
-                {
-                    name: "description",
-                    type: "text",
-                    isNullable: true
-                },
-                {
-                    name: "position",
-                    type: "float"
-                },
-                {
-                    name: "created_at",
-                    type: "timestamp",
-                    default: "CURRENT_TIMESTAMP"
-                },
-                {
-                    name: "updated_at",
-                    type: "timestamp",
-                    default: "CURRENT_TIMESTAMP",
-                    onUpdate: "CURRENT_TIMESTAMP"
-                }
-            ]
-        }), true);
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS tasks (
+                id SERIAL PRIMARY KEY,
+                column_id INT NOT NULL,
+                title VARCHAR NOT NULL,
+                description TEXT,
+                position FLOAT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("tasks");
+        await queryRunner.query(`
+            DROP TABLE IF EXISTS tasks
+        `);
     }
-
 }
