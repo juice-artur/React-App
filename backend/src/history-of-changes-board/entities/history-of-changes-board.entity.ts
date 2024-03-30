a89c52a97173bde7e7e0e1db7d30f661ee5a8e6b
juice-artur <asasin.lol10@gmail.com>
2024-03-30 11:58:58 +0200

    Created new entity for board

--- backend/src/history-of-changes-board/entities/history-of-changes-board.entity.ts
+++ backend/src/history-of-changes-board/entities/history-of-changes-board.entity.ts
@@ -1,19 +1,30 @@
 import { AutoMap } from "@automapper/classes";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from "@nestjs/swagger";
+import { Board } from "src/board/entities/board.entity";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({name: "history_of_changes_board"})
 export class HistoryOfChangesBoard 
 {
     @AutoMap()
     @PrimaryGeneratedColumn()
+    @ApiProperty()
     id: number;
 
     @AutoMap()
     @Column({ type: "text" })
+    @ApiProperty()
     description: string;
 
     @AutoMap()
     @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    @ApiProperty()
     created_at: Date;
+    
+    @AutoMap()
+    @ManyToOne(() => Board)
+    @JoinColumn({ name: "board_id" })
+    @ApiProperty()
+    board: Board;
 }
 
